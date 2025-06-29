5e4a8ef68684f5c648042fc0d4c21ca89bd88f6a
pavisai2005 <ppavii2005@gmail.com>
2025-06-29 23:33:07 +0530

    Nunit and moq

---
+++ namespace Assingment_NUnit.cs
@@ -0,0 +1,15 @@
+namespace Assignment_05_NUnit
+{
+    public class MathUtility
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
